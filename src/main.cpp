9b2fa4253dc5e6431385f3f5c5d66c8017bb561b
RoniR <ronirachid459@gmail.com>
2026-08-09 17:33:36 +0200

    read angle function added for AS5600

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+long moniter_speed = 115200;
+
+#include <Wire.h>
+
+#define AS5600_ADDR 0x36 // device address
+#define RAW_ANGLE_MSB 0x0C // register address
+
+// reads the raw angle
+uint16_t readRawAngle() {
+  Wire.beginTransmission(AS5600_ADDR);
+  Wire.write(RAW_ANGLE_MSB);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(AS5600_ADDR, 2);
+  if (Wire.available() >= 2) {
+    uint8_t highByte = Wire.read();
+    uint8_t lowByte  = Wire.read();
+
+    return ((highByte & 0x0F) << 8) | lowByte;
+  }
+  return 0;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(moniter_speed);
+  Wire.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  uint16_t rawAngle = readRawAngle();
+  float degrees = (rawAngle / 4096.0) * 360.0;
+  
+  Serial.print("Angle: ");
+  Serial.print(degrees);
+  
+  delay(100);
 }
