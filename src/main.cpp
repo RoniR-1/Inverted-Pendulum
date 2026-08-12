f348a1c487d7400acc3b40a237c08045875e89ad
RoniR <ronirachid459@gmail.com>
2026-08-12 17:59:00 +0200

    AS5600 debugging

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@
 
   #define AS5600_ADDR 0x36 // device address, A4 for data, A5 for clock as per Wire.h
   #define RAW_ANGLE_MSB 0x0C // register address
-
+  #define STATUS_REG    0x0B
   long moniter_speed = 115200;
   volatile uint8_t hault = 0;
-  uint16_t motorSpeed = 200;
-  const float targetAngle = 143.35;
-  const float safeMargin = 25;
-  const int availableSpace = 10; // in cm
+  uint16_t motorSpeed = 200; //DIR true means its going towards the motor
+  float targetAngle = 144.67;
+  const float safeMargin = 10;
+  const int availableSpace = 90; // in cm
 
   volatile unsigned long lastInteruptTime = 0;
-  int differenceTimeAllowed = 50;
+  int differenceTimeAllowed = 200;
 
   // multiple interupts could fire from a single physical button/switch/etc
   // to prevent this, interupts can at most fire every 50 miliseconds
@@ -35,14 +35,12 @@
 
   // reads the raw angle from AS5600, converts it to a range between 0 and 360
   float readAngle() {
-    Serial.println("inANgle");
     Wire.beginTransmission(AS5600_ADDR);
     Wire.write(RAW_ANGLE_MSB);
     if (Wire.endTransmission() != 0) {
-    Serial.println("I2C Error: AS5600 not responding!");
+      Serial.println("Angle does not respond");
     return -1.0f; // Return error instead of freezing
-  }
-  
+    }
     
     Wire.requestFrom(AS5600_ADDR, 2);
     if (Wire.available() >= 2) {
@@ -71,17 +69,41 @@
 
   /*move the carriage the away from the motor until a calibar pin is HIGH*/
   bool calibrate() {
-    Serial.println("Calibrate");
-    Serial.print(digitalRead(CALIBAR_PIN));
+    Serial.print("Calibrate");
+    Serial.println(digitalRead(CALIBAR_PIN));
+    // targetAngle = readAngle();
+    // delay(2000);
+    // if (targetAngle <=180) targetAngle += 180;
+    // else targetAngle -= 180;
+    // Serial.println(targetAngle);
+    // delay(2000);
     while(digitalRead(CALIBAR_PIN) != HIGH) {
-      stepMotor(16, true, 300);
+      stepMotor(16, false, 300);
     }
     return true;
+  }
+
+  uint8_t readStatus() {
+  Wire.beginTransmission(AS5600_ADDR);
+  Wire.write(STATUS_REG);
+  Wire.endTransmission();
+  Wire.requestFrom(AS5600_ADDR, 1);
+  if (Wire.available()) {
+    return Wire.read();
+  }
+  return 0;
   }
 
   void setup() {
     Serial.begin(moniter_speed);
     Wire.begin();
+    Wire.setWireTimeout(3000, true);
+
+    uint8_t status = readStatus();
+    if (status & 0x20) Serial.println("AS5600 Status: Magnet too strong (too close)");
+    else if (status & 0x10) Serial.println("AS5600 Status: Magnet too weak (too far)");
+    else if (status & 0x08) Serial.println("AS5600 Status: Magnet detected OK!");
+    else Serial.println("AS5600 Status: Magnet NOT detected!");
 
     pinMode(STEP_PIN, OUTPUT);
     pinMode(DIR_PIN, OUTPUT);
@@ -100,11 +122,12 @@
     }
   }
 
+
   void loop() {
     Serial.println("insideLoop"); ; // hault the code
     while(hault > 0) {
       Serial.println("Halting"); ; // hault the code
-      Serial.println(digitalRead(TOGGLE_PIN));
+      digitalWrite(EN_PIN, HIGH); // disable motor
       delay(100);
     }
 
@@ -116,6 +139,9 @@
     */
     if (errorAngle < -180) errorAngle += 360;
     if (errorAngle > 180) errorAngle -= 360;
+    Serial.println(errorAngle);
+    Serial.print("now the real one");
+    Serial.print(realAngle);
 
     if (abs(errorAngle) > safeMargin) {
       digitalWrite(EN_PIN, HIGH); // Disable motor output
@@ -123,6 +149,5 @@
     } else {
       digitalWrite(EN_PIN, LOW); // Re-enable motor
     }
-    
     stepMotor(16 * abs(errorAngle), errorAngle > 0 ? true : false, motorSpeed);
   }
